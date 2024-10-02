b951dd96f92920db5dcc3a88304b6dbf75b12a6c
Kinhashi <serranokian8@gmail.com>
2024-10-02 01:08:40 -0700

    Update index (1).php

--- index (1).php
+++ index (1).php
@@ -45,7 +45,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KFC Inventory System</title>
+    <title>Kentucky Fried Chicken Inventory System</title>
     <link rel="stylesheet" href="style3.css">
     <style>
         body { font-family: Arial, sans-serif; }
